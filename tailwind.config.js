f4ad7f1752bdbd666e436c8b702a45a171d07c1b
O-Hammam <omarhammam94@gmail.com>
2025-01-19 15:08:48 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        english: ["Charm", "serif"],
+        englishTitle: ["Great Vibes", "serif"],
+        arabic: ["Aref Ruqaa", "serif"],
+        arabicTitle: ["Aref Ruqaa", "serif"],
+      },
+      colors: {
+        gold: {
+          light: "#F5E6BE",
+          DEFAULT: "#D4AF37",
+          dark: "#B4941F",
+        },
+        sage: {
+          light: "#E1E5D6",
+          DEFAULT: "#9CAF88",
+          dark: "#708060",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
