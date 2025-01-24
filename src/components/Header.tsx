beb4edba8db7c0d97fbab172749e362436e3133c
O-Hammam <omarhammam94@gmail.com>
2025-01-24 20:16:19 +0100

    spell checked, vertical align

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,43 +12,41 @@ const Header: React.FC = () => {
         <img
           src="/assets/couple.webp"
           alt="Happy Couple"
-          className="w-full h-full object-cover mb-12"
+          className="w-full h-full object-cover mb-10"
         />
       </div>
       <div className="relative mb-2">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 relative items-center">
           <div className="flex justify-center items-center">
-            <h1 className="text-6xl font-englishTitle font-bold px-4 pt-2 text-gold-dark text-center drop-shadow-md">
+            <h1 className="text-6xl font-englishTitle font-bold px-4 mt-2 text-gold-dark text-center drop-shadow-md">
               Groom&nbsp;
               <br className="md:hidden" />
               &amp;&nbsp;Bride
             </h1>
           </div>
           <h1
-            className="reverse text-7xl font-arabicTitle font-bold inline-block px-4 pt-4 text-gold-dark text-center drop-shadow-md"
+            className="reverse text-7xl font-arabicTitle font-bold inline-block px-4 mt-8 text-gold-dark text-center drop-shadow-md"
             dir="rtl"
           >
-            عُمَر وَسَلْمَىٰ
+            اسم الزوجين
           </h1>
         </div>
       </div>
       <div className="pb-2 space-y-2">
         <div className="mb-4 drop-shadow-sm">
           <p className="text-lg text-center text-jetText">───── ⋆⋅☆⋅⋆ ─────</p>
-          <h2 className="text-5xl font-arabicTitle font-bold text-center text-gold-dark">
+          <h2 className="mt-2 text-5xl font-arabicTitle font-bold text-center text-gold-dark">
             دعوة زفاف
           </h2>
-          <h2 className="font-englishTitle text-2xl font-semibold mt-2 text-center text-gold-dark">
+          <h2 className="font-english text-2xl font-semibold my-2 text-center text-gold-dark">
             Wedding Invitation
           </h2>
           <p className="text-lg text-center text-jetText">───── ⋆⋅☆⋅⋆ ─────</p>
         </div>
-        <p className="font-arabicAmiri text-2xl text-center text-jetText">
-          نتـشـرف بدعـوتـكم لتـشاركــونا فـرحـتنا
-        </p>
-        <p className="font-arabicAmiri text-2xl text-center text-jetText">
-          وذلـك بمـشــيـئـة الله تـعــالى
-        </p>
+        <div className="font-arabicAmiri space-y-2 text-2xl text-center text-jetText">
+          <p>نتـشـرف بدعـوتـكم لتـشاركــونا فـرحـتنا</p>
+          <p>وذلك بمـشــيـئـة الله تـعــالى</p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,31 @@ export default {
     extend: {
       fontFamily: {
         english: ["Charm", "serif"],
-        englishTitle: ["Great Vibes", "serif"],
+        englishSubTitle: ["Great Vibes", "serif"],
+        englishTitle: ["Gwendolyn", "serif"],
         arabic: ["Aref Ruqaa", "serif"],
         arabicTitle: ["Aref Ruqaa", "serif"],
         arabicAmiri: ["Amiri", "serif"],
       },
       colors: {
         gold: {
-          light: "#F5E6BE",
+          light: "#F8EED1",
+          // light: "#F5E6BE",
           DEFAULT: "#D4AF37",
           dark: "#B4941F",
         },
         sage: {
-          light: "#E0E9C9",
+          light: "#F8F8F4",
+          // light: "#E0E9C9",
           DEFAULT: "#9CAF88",
-          dark: "#363E2E",
+          // dark: "#363E2E",
+          dark: "#E7E1C5",
         },
         jetText: {
           DEFAULT: "#2C2C2C",
+        },
+        edge: {
+          DEFAULT: "#F8EED1",
         },
       },
     },
