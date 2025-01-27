1632eea561b27bf85997628eabec67246e6314b4
O-Hammam <omarhammam94@gmail.com>
2025-01-27 01:28:51 +0100

    added initial animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Header.css"; // Import the CSS file for animations
 
 const Header: React.FC = () => {
+  const [showNames, setShowNames] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const timerNames = setTimeout(() => {
+      setShowNames(true);
+    }, 1000); // Delay before showing names
+
+    const timerImage = setTimeout(() => {
+      setShowImage(true);
+    }, 500); // Delay before showing image
+
+    return () => {
+      clearTimeout(timerNames);
+      clearTimeout(timerImage);
+    };
+  }, []);
+
   return (
     <div className="mb-6">
       <img
@@ -8,7 +27,11 @@ const Header: React.FC = () => {
         alt="Bismillah"
         className="w-40 h-auto mx-auto object-cover mb-2 py-2 border-b border-gold"
       />
-      <div className="w-64 h-auto mx-auto transform hover:scale-105 transition-transform duration-300">
+      <div
+        className={`w-64 h-auto mx-auto transform hover:scale-105 transition-transform duration-300 ${
+          showImage ? "slide-down" : "hidden"
+        }`}
+      >
         <img
           src="/assets/couple.webp"
           alt="Happy Couple"
@@ -18,21 +41,27 @@ const Header: React.FC = () => {
       <div className="relative mb-2 grid grid-cols-1 justify-items-center">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 relative items-center max-w-4xl">
           <div className="flex justify-center items-center">
-            <h1 className="text-6xl font-englishTitle font-bold px-4 mt-2 text-gold-dark text-center drop-shadow-md">
+            <h1
+              className={`text-7xl leading-[0.8] font-englishTitle font-bold px-4 mt-3 text-gold-dark text-center drop-shadow-md ${
+                showNames ? "slide-down" : "hidden"
+              }`}
+            >
               Groom&nbsp;
               <br className="md:hidden" />
               &amp;&nbsp;Bride
             </h1>
           </div>
           <h1
-            className="reverse text-7xl font-arabicTitle font-bold inline-block px-4 mt-8 text-gold-dark text-center drop-shadow-md"
+            className={`reverse text-7xl font-arabicTitle font-bold inline-block px-4 mt-8 text-gold-dark text-center drop-shadow-md ${
+              showNames ? "slide-down" : "hidden"
+            }`}
             dir="rtl"
           >
             اسم الزوجين
           </h1>
         </div>
       </div>
-      <div className="pb-2 space-y-2">
+      <div className={`pb-2 space-y-2 ${showNames ? "slide-down" : "hidden"}`}>
         <div className="mb-4 drop-shadow-sm">
           <p className="text-lg text-center text-jetText">───── ⋆⋅☆⋅⋆ ─────</p>
           <h2 className="mt-2 text-5xl font-arabicTitle font-bold text-center text-gold-dark">
